5c4c0101d9968fff128e23b7682376d13fa3dee8
JOSPHAT-KITUKU <107811020+JOSPHAT-KITUKU@users.noreply.github.com>
2023-04-17 12:17:08 +0300

    Changing the Del function to handle the setPrev eval

--- src/App.js
+++ src/App.js
@@ -36,6 +36,8 @@ function App() {
     }
     const num = val.slice(0, -1);
     setResult(num);
+     // eslint-disable-next-line
+    setPrev(eval(num).toString());
   }
   return (
     <div className='App-wrapper'>
